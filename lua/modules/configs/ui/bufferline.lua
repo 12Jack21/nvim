3c4b34cffe9da1587f07ab5c88e68801507c14e3
12Jack21 <michealgray3367@hotmail.com>
2023-06-04 12:26:08 +0800

    doc: update issues chore: make install.sh available feat(ui): bufferline and lualine extend functions

--- lua/modules/configs/ui/bufferline.lua
+++ lua/modules/configs/ui/bufferline.lua
@@ -1,26 +1,42 @@
 return function()
 	local icons = { ui = require("modules.utils.icons").get("ui") }
-
+	local bufferline = require("bufferline")
 	local opts = {
 		options = {
-			number = nil,
+			-- number = nil,
+			numbers = function(opts)
+				-- return string.format("%s.%s", opts.lower(opts.id), opts.lower(opts.ordinal))
+				return string.format("%s", opts.lower(opts.id))
+			end,
+
+			mode = "buffers", -- set to "tabs" to only show tabpages instead
 			modified_icon = icons.ui.Modified,
 			buffer_close_icon = icons.ui.Close,
 			left_trunc_marker = icons.ui.Left,
 			right_trunc_marker = icons.ui.Right,
 			max_name_length = 14,
 			max_prefix_length = 13,
-			tab_size = 20,
+			tab_size = 18, -- 20
 			color_icons = true,
 			show_buffer_icons = true,
 			show_buffer_close_icons = true,
 			show_close_icon = true,
 			show_tab_indicators = true,
+			-- style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+			themable = false,
 			enforce_regular_tabs = true,
 			persist_buffer_sort = true,
 			always_show_bufferline = true,
-			separator_style = "thin",
+			indicator = {
+				icon = "▎", -- this should be omitted if indicator style is not 'icon'
+				icon = "🏀",
+				icon = "🚀",
+				-- style = "underline",
+				style = "icon",
+			},
+			separator_style = "thick",
 			diagnostics = "nvim_lsp",
+			-- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
 			diagnostics_indicator = function(count)
 				return "(" .. count .. ")"
 			end,
@@ -39,10 +55,34 @@ return function()
 					padding = 1,
 				},
 			},
+			-- should turn on 'mousemoveevent', but has unneeded overhead
+			-- hover = {
+			-- 	enabled = true,
+			-- 	delay = 200,
+			-- 	reveal = { "close" },
+			-- },
+			sort_by = "insert_after_current",
 		},
 		-- Change bufferline's highlights here! See `:h bufferline-highlights` for detailed explanation.
 		-- Note: If you use catppuccin then modify the colors below!
-		highlights = {},
+		highlights = {
+			indicator_selected = { -- tab selected, underline
+				fg = "#f24f72", -- TODO: do not work
+				sp = "#f24f72",
+				-- bg = "bg",
+				bold = true,
+			},
+			buffer_selected = { -- tab selected, underline
+				fg = "#f24f72", -- TODO: do not work
+				sp = "#f24f72",
+				-- bg = "bg",
+				bold = true,
+			},
+			tab_close = {
+				fg = "#ccccee",
+				bg = "#ec5f67",
+			},
+		},
 	}
 
 	if vim.g.colors_name:find("catppuccin") then
@@ -68,5 +108,5 @@ return function()
 		opts = vim.tbl_deep_extend("force", opts, catppuccin_hl_overwrite)
 	end
 
-	require("bufferline").setup(opts)
+	bufferline.setup(opts)
 end
